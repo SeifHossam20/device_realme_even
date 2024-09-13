b35ed10a9f8c18abafc89d62865ce0f479071f00
SeifHossam20 <seifhossam862@gmail.com>
2024-09-14 00:30:42 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -69,19 +69,16 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1 \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.1-impl \
-    libbt-vendor \
-    libldacBT_bco
 
 PRODUCT_PACKAGES += \
-    libbtconfigstore
+
 
 # Camera
 PRODUCT_PACKAGES += \
-    Camera
+
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-service \
     android.hardware.configstore@1.1-service
 
 # Display
@@ -89,7 +86,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.common@1.2 \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     libvulkan
@@ -107,8 +103,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.even \
-    android.hardware.health-service.even-recovery
+
 
 # HIDL
 PRODUCT_PACKAGES += \
@@ -165,7 +160,6 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
-    com.gsma.services.nfc  \
     NfcNci \
     SecureElement \
     Tag
